fdd2667dd129f72af215bcaae013b8ae03f2a398
farrelvarian <42968960+farrelvarian@users.noreply.github.com>
2018-09-04 20:12:22 +0700

    Add files via upload

---
+++ tugas1/src/tugas1/Tugas1.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas1;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Tugas1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int jumlah = 5;
+        for (int a=0;a<jumlah;a++){
+            for (int b=0;b<=a;b++){
+                System.out.print("X");
+            }System.out.println();
+        }
+    }
+    
+}
